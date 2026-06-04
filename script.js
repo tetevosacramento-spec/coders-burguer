ea7e12797e6c3977d0ae8b102ce33e014a3ad010
Jhonatan Serafim <jhonatan.serafim@gmail.com>
2026-06-04 19:06:48 -0300

    feat: adjust header, banner and footer

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('oi');
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.main-header');
+  if (!header) return;
+
+  if (window.scrollY > 50) {
+    header.classList.add('shadow-lg');
+  } else {
+    header.classList.remove('shadow-lg');
+  }
+});
